979d893b9b806685994e410b9568332402a4d22e
Ryo Okazaki <ryo.okazaki.0929@gmail.com>
2017-06-07 22:21:22 +0900

    Fix a bug

--- scripts/motors_enable.py
+++ scripts/motors_enable.py
@@ -8,4 +8,5 @@ if __name__ == '__main__':
     rospy.wait_for_service('/motor_off')
     rospy.on_shutdown(rospy.ServiceProxy('/motor_off', Trigger).call)
     rospy.ServiceProxy('/motor_on', Trigger).call()
+    rospy.init_node('enable_motors')
     rospy.spin()
