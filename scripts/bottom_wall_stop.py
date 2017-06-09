8807a718868588cca272f200564841ee6b004de0
Ryo Okazaki <ryo.okazaki.0929@gmail.com>
2017-06-09 17:31:40 +0900

    Add a script for enable to stop in front of the bottom wall

---
+++ scripts/bottom_wall_stop.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import rospy, copy
+from geometry_msgs.msg import Twist
+from std_srvs.srv import Trigger, TriggerResponse
+from pimouse_ros.msg import LightSensorValues
+
+class BottomWallStop():
+    def __init__(self):
+        self.cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+
+        self.sensor_values = LightSensorValues()
+        rospy.Subscriber('/lightsensors', LightSensorValues, self.callback)
+
+    def callback(self, messages):
+        self.sensor_values = messages
+
+    def run(self):
+        rate = rospy.Rate(10)
+        data = Twist()
+
+        while not rospy.is_shutdown():
+            data.linear.x = 0.0 if self.sensor_values.sum_all > 500
+            self.cmd_vel.publish(data)
+            rate.sleep()
+
+if __name__ == '__main__':
+    rospy.init_node('bottom_wall_stop')
+    rospy.wait_for_service('/motor_on')
+    rospy.wait_for_service('/motor_off')
+    rospy.on_shutdown(rospy.ServiceProxy('/motor_off', Trigger).call)
+    rospy.ServiceProxy('/motor_on', Trigger).call()
+    BottomWallStop().run()
